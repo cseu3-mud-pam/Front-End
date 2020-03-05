8efdf41d4b1addec64e12974432c6e2802f0d263
Modurotolu Olokode <durolawk@gmail.com>
2020-03-05 14:20:44 +0100

    completed registration endpoint

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function Register() {
+const initialRegistrationFeild = {
+  username: '',
+  email: '',
+  password1: '',
+  password2: ''
+}
+
+function Register(props) {
+  const [registrationField, setRegistrationField] = useState(initialRegistrationFeild)
+
+  const handlechange = (e) => {
+    setRegistrationField({
+      ...registrationField,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    axios.post('https://mudgame-pam.herokuapp.com/api/registration/', registrationField)
+      .then(res => {
+        localStorage.setItem('token', res.data.key)
+        props.history.push('/login')
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   return (
     <div>
         <div>Register</div>
         <form>
-            <input placeholder='username' />
-            <input placeholder='password' />
-            <input placeholder='password2' />
-            <Link to='/play'><button>Submit</button></Link>
+            <input placeholder='username' name='username' onChange={handlechange} />
+            <input placeholder='email' name='email' onChange={handlechange} />
+            <input placeholder='password' name='password1' onChange={handlechange} />
+            <input placeholder='password2' name='password2' onChange={handlechange} />
+            <button onClick={handleLogin}>Submit</button>
         </form>
     </div>
   );
